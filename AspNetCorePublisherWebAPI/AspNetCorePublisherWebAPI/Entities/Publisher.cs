a622f0e5264fd9673fe143db7f55d9f3a63a0b57
Khoa Tran <khoakom@gmail.com>
2020-09-16 16:09:45 +0700

    Part 3: Creating an Entity Framework Service
    9. Entity Classes

---
+++ AspNetCorePublisherWebAPI/AspNetCorePublisherWebAPI/Entities/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCorePublisherWebAPI.Entities
+{
+    public class Publisher
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        public int Established { get; set; }
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
