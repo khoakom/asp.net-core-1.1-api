1e95437d2c063987abed67fe0633dcb5c6c8ac4c
Khoa Tran <khoakom@gmail.com>
2020-09-16 11:47:02 +0700

    Part 2: ASP.NET Core 1.1 Web API Using In-Memory Data
    5. Adding In-Memory Data Storage

---
+++ AspNetCorePublisherWebAPI/AspNetCorePublisherWebAPI/Models/PublisherDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCorePublisherWebAPI.Models
+{
+    public class PublisherDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Established { get; set; }
+        public ICollection<BookDTO> Books { get; set; } = new List<BookDTO>();
+        public int BookCount { get { return Books.Count; } }
+    }
+}
